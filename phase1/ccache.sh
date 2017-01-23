7ce7c867e3c27bdbcf5c79cfc449feaf3f19b1f5
Jo-Philipp Wich <jo@mein.io>
2017-01-23 14:21:41 +0100

    phase1: ccache.sh: avoid creating nested symlink
    Signed-off-by: Jo-Philipp Wich <jo@mein.io>

--- phase1/ccache.sh
+++ phase1/ccache.sh
@@ -11,9 +11,11 @@ grep -sq compiler_check "$HOME/.ccache/ccache.conf" || \
 	echo "compiler_check = %compiler% -dumpmachine; %compiler% -dumpversion" >> "$HOME/.ccache/ccache.conf"
 
 for dir in $(make --no-print-directory val.TOOLCHAIN_DIR val.STAGING_DIR val.STAGING_DIR_HOST V=s | grep staging_dir/); do
-	mkdir -p "$dir"
-	test -L "$dir/ccache" || rm -rf "$dir/ccache"
-	ln -s "$HOME/.ccache" "$dir/ccache"
+	if [ ! -L "$dir/ccache" ]; then
+		mkdir -vp "$dir"
+		rm -vrf "$dir/ccache"
+		ln -vs "$HOME/.ccache" "$dir/ccache"
+	fi
 done
 
 ./staging_dir/host/bin/ccache -s 2>/dev/null
