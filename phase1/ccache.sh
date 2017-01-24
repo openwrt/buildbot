c5ed9101014be9ea899bf660583ad51b600fb2ab
Jo-Philipp Wich <jo@mein.io>
2017-01-24 14:47:33 +0100

    phase1: ccache.sh: also overwrite dangling symlinks
    Signed-off-by: Jo-Philipp Wich <jo@mein.io>

--- phase1/ccache.sh
+++ phase1/ccache.sh
@@ -11,7 +11,7 @@ grep -sq compiler_check "$HOME/.ccache/ccache.conf" || \
 	echo "compiler_check = %compiler% -dumpmachine; %compiler% -dumpversion" >> "$HOME/.ccache/ccache.conf"
 
 for dir in $(make --no-print-directory val.TOOLCHAIN_DIR val.STAGING_DIR val.STAGING_DIR_HOST V=s | grep staging_dir/); do
-	if [ ! -L "$dir/ccache" ]; then
+	if [ ! -L "$dir/ccache" ] || [ -L "$dir/ccache" -a ! -d "$dir/ccache" ]; then
 		mkdir -vp "$dir"
 		rm -vrf "$dir/ccache"
 		ln -vs "$HOME/.ccache" "$dir/ccache"
