cc9df2f066bcee0aeb64769d18e9f6a4e5d152b9
Jo-Philipp Wich <jo@mein.io>
2017-12-06 15:54:11 +0100

    phase1: ccache.sh: fail on error
    Signed-off-by: Jo-Philipp Wich <jo@mein.io>

--- phase1/ccache.sh
+++ phase1/ccache.sh
@@ -2,19 +2,19 @@
 
 export LC_ALL=C
 
-mkdir -p "$HOME/.ccache"
+mkdir -p "$HOME/.ccache" || exit 1
 
 grep -sq max_size "$HOME/.ccache/ccache.conf" || \
-	echo "max_size = 10.0G" >> "$HOME/.ccache/ccache.conf"
+	echo "max_size = 10.0G" >> "$HOME/.ccache/ccache.conf" || exit 1
 
 grep -sq compiler_check "$HOME/.ccache/ccache.conf" || \
-	echo "compiler_check = %compiler% -dumpmachine; %compiler% -dumpversion" >> "$HOME/.ccache/ccache.conf"
+	echo "compiler_check = %compiler% -dumpmachine; %compiler% -dumpversion" >> "$HOME/.ccache/ccache.conf" || exit 1
 
 for dir in $(make --no-print-directory val.TOOLCHAIN_DIR val.STAGING_DIR val.STAGING_DIR_HOST V=s | grep staging_dir/); do
 	if [ ! -L "$dir/ccache" ] || [ -L "$dir/ccache" -a ! -d "$dir/ccache" ]; then
-		mkdir -vp "$dir"
-		rm -vrf "$dir/ccache"
-		ln -vs "$HOME/.ccache" "$dir/ccache"
+		mkdir -vp "$dir" || exit 1
+		rm -vrf "$dir/ccache" || exit 1
+		ln -vs "$HOME/.ccache" "$dir/ccache" || exit 1
 	fi
 done
 
